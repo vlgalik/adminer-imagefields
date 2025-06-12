66781fdd218574503321dc20b59feed8d632170e
Ladislav Gálik <821786+vlgalik@users.noreply.github.com>
2025-06-12 14:48:54 +0200

    Update imagefields.php
    - links replaced for images using JavaScript instead of PHP
    - works in SQL command section

--- imagefields.php
+++ imagefields.php
@@ -11,25 +11,41 @@ class AdminerImagefields
 {
     private $width, $height, $url;
 
-    public static $template = '<img src="%s" style="display: block; max-width: %spx; max-height: %spx;" alt="%s"/>%s';
-
     function __construct($width = 200, $height = 200, $url = false) {
         $this->width = $width;
         $this->height = $height;
         $this->url = $url;
     }
 
-    public function selectVal(&$val, $link, $field, $original) {
+    function head(?bool $dark = null): bool {
 
-        $trimmed = trim($val);
+        echo sprintf('<script %s>', Adminer\nonce());
+        echo sprintf("        
+            (function () {
+                document.addEventListener('DOMContentLoaded', function (e) {
+                    const links = document.querySelectorAll('.scrollable table tbody a');
+                    const re = /^.*\.(jpg|jpeg|png|gif)(\?.*)?$/
+            
+                    for (let i = 0; i < links.length; i++) {
+                        if (re.test(links[i].href)) {
+                            const img = document.createElement('img');
+                            img.src = links[i].href;
+                            img.alt = links[i].href;
+                            img.style.display = 'block';
+                            img.style.maxWidth = '%spx';
+                            img.style.maxHeight = '%spx';
+            
+                            img.addEventListener('load', function (image) {
+                                %s
+                                links[this].insertBefore(image, links[this].firstChild);
+                            }.bind(i, img));
+                        }
+                    }
+                });
+            })();
+        ", $this->width, $this->height, $this->url ? '' : "links[this].innerHTML = '';");
+        echo '</script>';
 
-        if ($link !== null && self::hasImageExtension($trimmed)) {
-            $val = sprintf(self::$template, $trimmed, $this->width, $this->height, $val, $this->url ? $val : '');
-        }
+        return true;
     }
-
-    private static function hasImageExtension($input) {
-        return preg_match('/^.*\.(jpg|jpeg|png|gif)(\?.*)?$/i', $input);
-    }
-
 }
